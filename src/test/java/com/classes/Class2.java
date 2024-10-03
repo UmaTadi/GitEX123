c7bd88cf930477d91021e5b2f7c8bf5423e4951e
UmaTadi <uma.tadi1997@gmail.com>
2024-10-03 13:18:05 +0530

    i added and removed classes

---
+++ src/test/java/com/classes/Class2.java
@@ -0,0 +1,9 @@
+package com.classes;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+System.out.println("This is cLASS2");
+	}
+
+}
---
+++ src/test/java/com/classes/Class4.java
@@ -0,0 +1,9 @@
+package com.classes;
+
+public class Class4 {
+
+	public static void main(String[] args) {
+		System.out.println("This is class4");
+	}
+
+}
